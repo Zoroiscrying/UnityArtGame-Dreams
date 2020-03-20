1ac998dc182e299b6df482022461c836157593f4
Zoroiscrying <zoroiscrying@outlook.com>
2020-03-20 15:46:58 +0800

    Into the Dream Level logic completed

--- DreamsByYanAndJiang/Assets/Scripts/InteractionSys/StandingCollapsePlatform.cs
+++ DreamsByYanAndJiang/Assets/Scripts/InteractionSys/StandingCollapsePlatform.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StandingCollapsePlatform : TouchableItem
 {
+    private Material _thisMaterial;
+    private Shader _thisShader;
     private float _beginTouchTime;
     private bool _beginTouch = false;
     [SerializeField] private float _collapseTime = 2f;
     [SerializeField] private float _recoverTime = 2f;
     private float _percentage = 0.0f;
+
+    private void Start()
+    {
+        _thisShader = ResourceManager.Instance.StandingCollapsePlatformShader;
+       _thisMaterial = this.GetComponent<Renderer>().sharedMaterial = new Material(_thisShader);
+       if (_thisMaterial.HasProperty("_EmissionIntensity"))
+       {
+           _thisMaterial.SetFloat("_EmissionIntensity",0.1f);
+       }
+    }
 
     public override void OnBeginTouch()
     {
@@ -24,6 +37,11 @@ public class StandingCollapsePlatform : TouchableItem
 
     private void UpdateGraphics()
     {
+        if (_thisMaterial.HasProperty("_EmissionIntensity"))
+        {
+            _thisMaterial.SetFloat("_EmissionIntensity",0.1f+_percentage*1.2f);
+        }
+        
         
     }
 
@@ -33,11 +51,13 @@ public class StandingCollapsePlatform : TouchableItem
         this.GetComponent<Renderer>().enabled = false;
         _beginTouch = false;
         Timer.Register(_recoverTime, Recover);
+        _percentage = 0.0f;
     }
 
     private void Recover()
     {
         _beginTouch = false;
+        _percentage = 0.0f;
         this.GetComponent<Collider>().enabled = true;
         this.GetComponent<Renderer>().enabled = true;
     }
---
+++ DreamsByYanAndJiang/Assets/Scripts/Singleton/ResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : Singleton<ResourceManager>
+{
+    public Shader StandingCollapsePlatformShader;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
